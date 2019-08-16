7dd3ef598cbc5f5403cfbd6999289f235e5a82a9
Oleg Jakunin <oleg.kemz@gmail.com>
2019-08-16 14:55:23 +0300

    Reading input data functionality

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
 var budgetController = (function () {
-  var x = 23;
 
-  var add = function (a) {
-    return a + x;
-   }
-
-   return {
-     publicTest: function(b) {
-       return add(b);
-      }
-   }
 })();
 
 
 var UIController = (function () {
 
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value,
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+
+    getDOMStrings: function () {
+      return DOMStrings;
+    }
+  };
+
 })();
 
 var controller = (function (budgetCtrl, UICtrl) {
+
+  var DOM = UICtrl.getDOMStrings();
+
+  var ctrlAddItem = function () {
+
+    // 1. Getting the input data from UI
+    var input = UICtrl.getInput();
+    // console.log(input);
+  }
+
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function (event) {
+    if (event.keyCode == 13 || event.which == 13) {
+      ctrlAddItem();
+    }
+  });
 
 })(budgetController, UIController);
